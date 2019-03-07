917d7d9327d84b43d4352574ebd84b446466de25
Paul Andrel <stavlor@stavlor.net>
2019-03-07 16:26:00 -0500

    Testing Attachment support

--- cogs/database.py
+++ cogs/database.py
@@ -16,7 +16,7 @@ class Database(commands.Cog):
             attach_url = list()
             for attach in message.attachments:
                 attach_url.append(attach.url)
-        sqlstatement = "INSERT INTO pm_tracking (user_id, user_name, message, attachment_url) VALUES ('{user_id}', '{user_name}{user_discriminator}', '{message}', '{attachment_url}')".format(user_id=message.author.id, user_name=message.author.name, message=message.content, user_discriminator=message.author.discriminator, attachment_url=attach_url);
+        sqlstatement = "INSERT INTO pm_tracking (user_id, user_name, message, attachment_url) VALUES ('{user_id}', '{user_name}{user_discriminator}', '{message}', '{attachment_url}')".format(user_id=message.author.id, user_name=message.author.name, message=message.content, user_discriminator=message.author.discriminator, attachment_url=attach_url[0]);
         await conn.execute(sqlstatement)
         await conn.close()
 
