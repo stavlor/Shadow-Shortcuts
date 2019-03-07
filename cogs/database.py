41c0b526b099196ddd38dfeb70bebd4f5b82bc17
Paul Andrel <stavlor@stavlor.net>
2019-03-07 16:39:30 -0500

    Wrap links in HTML

--- cogs/database.py
+++ cogs/database.py
@@ -15,7 +15,7 @@ class Database(commands.Cog):
         if hasattr(message, 'attachments'):
             attach_url = str()
             for attach in message.attachments:
-                attach_url += " "+str(attach.url)
+                attach_url += "<a href=\""+str(attach.url)+"\">Attachment</a> "
         sqlstatement = "INSERT INTO pm_tracking (user_id, user_name, message, attachment_url) VALUES ('{user_id}', '{user_name}{user_discriminator}', '{message}', '{attachment_url}')".format(user_id=message.author.id, user_name=message.author.name, message=message.content, user_discriminator=message.author.discriminator, attachment_url=attach_url);
         await conn.execute(sqlstatement)
         await conn.close()
