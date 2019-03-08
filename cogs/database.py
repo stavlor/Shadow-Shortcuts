b49736e0bb336587a53ac5498f7c49f9254dcf5d
Paul Andrel <stavlor@stavlor.net>
2019-03-08 14:08:23 -0500

    Fix ID lookup in database

--- cogs/database.py
+++ cogs/database.py
@@ -9,6 +9,18 @@ class Database(commands.Cog):
         bot.database = self
         bot.logger.info("Initialized Database cog")
 
+    @commands.command()
+    async def sql(self, ctx, *, arguments):
+        if not await self.bot.admin.can_run_command(ctx.author.roles, ['Shadow Guru', 'Moderators']):
+            await ctx.send("{ctx.author.mention} your not authorized to do that.".format(ctx=ctx))
+            return
+        self.bot.logger.info("SQL: {sql}".format(sql=sqlstatement))
+        conn = await asyncpg.connect(dsn="postgres://stavlorkaralain_gmail_com@localhost/bot", password="1234")
+        sql = str(arguments)
+        await conn.execute(sql)
+        await conn.close()
+
+
     async def log_direct_messages(self, message):
         conn = await asyncpg.connect(dsn="postgres://stavlorkaralain_gmail_com@localhost/bot", password="1234")
         attach_url = None
