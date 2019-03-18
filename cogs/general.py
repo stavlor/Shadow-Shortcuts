6889015d94a4f8461cf838609ea1ec964ca40993
Paul Andrel <stavlor@stavlor.net>
2019-03-18 13:57:29 -0400

    Update channel id.

--- cogs/general.py
+++ cogs/general.py
@@ -11,7 +11,7 @@ class General(commands.Cog):
     @commands.command(description="Send instructions on how to get Verified")
     async def verify(self, ctx, *, user: discord.Member = None):
         """How to get verified command."""
-        text = """Certain channels like #community-help require the Shadower role to allow you to view or chat in them, which is only given to people who verify that they are a current Shadow subscriber. To get verified, please DM any Shadow Guru or a Moderator a screenshot of https://account.shadow.tech/subscription to get your Shadower role.
+        text = """Certain channels like <#463782843898658846> require the Shadower role to allow you to view or chat in them, which is only given to people who verify that they are a current Shadow subscriber. To get verified, please DM any Shadow Guru or a Moderator a screenshot of https://account.shadow.tech/subscription to get your Shadower role.
 Note: Discord settings may prevent you from sending messages to those not on your friends list. Adjust this to allow messages to those you share a server with in order to be able to send the DM
 - For help with Discord Privacy settings see these images:
     - <https://cdn.discordapp.com/attachments/461203231339577356/557257216436994078/unknown.png>
