25bbe28634eb42b70d3f6c301f4ee75f8f5aaeda
Paul Andrel <stavlor@stavlor.net>
2019-04-18 12:06:28 -0400

    Fix After none.

--- cogs/database.py
+++ cogs/database.py
@@ -73,7 +73,9 @@ class Database(commands.Cog):
 
     async def process_member_update(self, before: discord.Member, after: discord.Member):
         if before.activity != after.activity:
-            if (after.activity.type is "ActivityType.playing") and (before.activity.type is not "ActivityType.playing"):
+            if after.activity is None:
+                self.bot.logger.info(f"Member is no longer playing: {before.activity.name}")
+            elif (after.activity.type is "ActivityType.playing") and (before.activity.type is not "ActivityType.playing"):
                 self.bot.logger.info(f"Member: {after.name} is now playing {after.activity.name}")
             elif before.activity is None:
                 self.bot.logger.info(f"Member {after.name} Game Change, Prior none new {after.activity.name}")
