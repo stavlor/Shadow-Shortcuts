8f2f4dcf0a528904db3613169acd44b7c0251c17
Paul Andrel <stavlor@stavlor.net>
2019-04-18 12:22:24 -0400

    Tweaks

--- cogs/database.py
+++ cogs/database.py
@@ -74,7 +74,7 @@ class Database(commands.Cog):
     async def process_member_update(self, before: discord.Member, after: discord.Member):
         if before.activity != after.activity:
             if after.activity is None:
-                self.bot.logger.info(f"Member {after.name} is no longer playing: {before.activity.name}")
+                self.bot.logger.info(f"Member {after.name} is no longer playing: {before.activity.name} Start: {before.activity.start} End: {before.activity.end}")
             elif before.activity is None:
                 self.bot.logger.info(f"Member {after.name} Game Change, Prior none new {after.activity.name}")
             elif (after.activity.type is "ActivityType.playing") and (before.activity.type is not "ActivityType.playing"):
