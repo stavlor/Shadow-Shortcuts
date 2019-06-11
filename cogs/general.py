edd371709e23a9ba2b79e1d6f9bb8651506b7d25
Paul Andrel <stavlor@stavlor.net>
2019-06-11 11:47:28 -0400

    Update drivers page.

--- cogs/general.py
+++ cogs/general.py
@@ -210,7 +210,7 @@ Send a clear **screenshot** of <https://account.shadow.tech/subscription> (click
     async def drivers(self, ctx, user: typing.Optional[discord.Member] = None):
         """Send current NVidia Drivers Info."""
         text = """**Current Nvidia Drivers for P5000** -- [__*US has only P5000s*__] [__*Non-US users may have GTX1080*__]
-          - Stable Drivers *(**Recommended**)*:  <https://www.nvidia.com/Download/driverResults.aspx/147869/en-us>
+          - Stable Drivers *(**Recommended**)*:  <https://www.nvidia.com/Download/driverResults.aspx/148432/en-us>
 
         **Notes:**
           - If running NVidia Drivers prior to 430.64 please ensure you update your drivers as there are critical CPU and other bugs in older drivers.
