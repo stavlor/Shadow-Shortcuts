8101bcd3c0290b07c383aec787cd719c50439b7d
Paul Andrel <stavlor@stavlor.net>
2019-07-08 13:24:30 -0400

    Bot url tweaks.

--- cogs/general.py
+++ cogs/general.py
@@ -452,7 +452,8 @@ For the Ghost user manual, see here: http://botstatic.stavlor.net/Ghost_Manual.p
         - <:WindowsShadow:555856447691292736>/**⌘** + **Alt** + **M** = Change mouse mode (Locked/Unlocked) (Locked is suggested for gaming)
         - <:WindowsShadow:555856447691292736>/**⌘** + **Alt** + **Q** = Quit Application
         - <:WindowsShadow:555856447691292736>/**⌘** + **Alt** + **F** = Toggle fullscreen
-        - <:WindowsShadow:555856447691292736>/**⌘** + **Alt** + **O** = Toggle Quick menu"""
+        - <:WindowsShadow:555856447691292736>/**⌘** + **Alt** + **O** = Toggle Quick menu
+        - <:WindowsShadow:555856447691292736>/**⌘** + **Alt** + **R** = Restart Streaming (Not in beta)"""
 
         if user is not None and await self.bot.admin.can_run_command(ctx.author.roles):
             text = f"From {ctx.author.name}\n{user.mention} {text}"
