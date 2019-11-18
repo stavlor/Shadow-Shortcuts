7c231286b2472642cb1332301aba0e8bb6aa39d3
Paul Andrel <stavlor@stavlor.net>
2019-11-18 13:07:36 -0500

    add a test command writelog

--- cogs/general.py
+++ cogs/general.py
@@ -709,7 +709,7 @@ Thanks for your interest, and we’ll have more information as soon as we can lo
         fp = open(file, 'w+')
         messages = await ctx.channel.history(limit=250).flatten()
         for item in messages:
-            fp.write(f"{item.content} - {item.created_at} - {item.mentions}\n")
+            fp.write(f"{item.clean_content} - {item.created_at} - {item.mentions}\n")
         fp.close()
 
 
