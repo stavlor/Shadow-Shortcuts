200f8813c8f82c69f7f0970af26cb18d34b88bb5
Paul Andrel <stavlor@stavlor.net>
2020-01-02 14:35:35 -0500

    Work Around for no self for decorators.

--- cogs/web.py
+++ cogs/web.py
@@ -2,19 +2,21 @@ from aiohttp import web
 from discord.ext import commands
 import discord
 import typing
-
+global botrefrence = None
 
 class Web(commands.Cog):
     def __init__(self, bot):
+        global botrefrence
         self.bot = bot
         bot.web = self
+        botrefrence = self.bot
         bot.logger.info("Initializing Web Cog")
         self.bot.web.routes = web.RouteTableDef()
         self.bot.web.application = web.Application()
         self.bot.web.application.add_routes(self.bot.web.routes)
         web.run_app(self.bot.web.application)
 
-    @self.bot.web.routes.get('/')
+    @botrefrence.web.routes.get('/')
     async def hello(self, request):
         return web.Response(text="Hello World")
 
