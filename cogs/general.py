3d966e6db15de9f9b9c2cd684c30139e9380ecc4
Paul Andrel <stavlor@stavlor.net>
2020-03-30 14:16:52 -0400

    Nvidia attempt 5

--- cogs/general.py
+++ cogs/general.py
@@ -354,7 +354,7 @@ For the Ghost user manual, see here: http://botstatic.stavlor.net/Ghost_Manual.p
     async def ping(self, ctx):
         """Ping command"""
         import datetime
-        if self.bot.admin.can_run_command(ctx.author.roles):
+        if await self.bot.admin.can_run_command(ctx.author.roles):
             now = datetime.datetime.utcnow()
             delta = (now - ctx.message.created_at).total_seconds()*1000
             await ctx.send('Pong! Server ping {:.3f}ms API ping: {:.3f}ms :ping_pong:'.format(delta, self.bot.latency*1000))
