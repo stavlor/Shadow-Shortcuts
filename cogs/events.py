e9c04b206bf2ab4f61755b734b6f4b7a57232bba
Paul Andrel <stavlor@stavlor.net>
2020-07-20 09:58:34 -0400

    Fix fetch_message()

--- cogs/events.py
+++ cogs/events.py
@@ -141,7 +141,8 @@ This article might be helpful: <https://www.extremetech.com/gaming/309320-riot-g
 
     @tasks.loop(minutes=5.0)
     async def check_status(self):
-        msg = await self.bot.fetch_message(553765896666087442)
+        channel = await self.bot.fetch_channel(633713376316620829)
+        msg = await channel.fetch_message(553765896666087442)
         if await self.bot.admin.get_status() == "All services operating normally":
             embed = discord.Embed(title="Shadow Status", url="https://status.shadow.tech", color=0x00ff00)
             embed.add_field(name="All services operating normally",
