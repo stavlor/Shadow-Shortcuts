acd7df671920e9c0e8a055588a87029515f94cc4
Paul Andrel <stavlor@stavlor.net>
2020-07-21 10:31:12 -0400

    fix tzinfo tzname request.

--- cogs/events.py
+++ cogs/events.py
@@ -141,14 +141,16 @@ This article might be helpful: <https://www.extremetech.com/gaming/309320-riot-g
 
     @tasks.loop(minutes=5.0)
     async def check_status(self):
+        from tzlocal import get_localzone
         from datetime import datetime
+        tz = get_localzone()
         curtime = datetime.now()
         msg = await self.bot.get_channel(633713376316620829).fetch_message(734769170847105114)
         if await self.bot.admin.get_status() == "All services operating normally":
             embed = discord.Embed(title="Shadow Status", url="https://status.shadow.tech", color=0x00ff00)
             embed.add_field(name="All services operating normally",
                             value="For additional status information please see the status page", inline=True)
-            embed.set_footer(text=f"Automatically updated message. Last Update was at {curtime.isoformat()} {curtime.tzinfo.tzname()}")
+            embed.set_footer(text=f"Automatically updated message. Last Update was at {curtime.isoformat()} {tz}")
             await msg.edit(embed=embed)
         else:
             status = await self.bot.admin.get_status()
