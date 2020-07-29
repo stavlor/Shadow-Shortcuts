7915f14c8b5c060f08f82e17cfb6a7428dcf9467
Paul Andrel <stavlor@stavlor.net>
2020-07-29 12:24:58 -0400

    Fix missing comma

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ initial_extensions = ['cogs.admin',
                       'cogs.events',
                       'cogs.general',
                       'cogs.schedevent',
-                      'cogs.web'
+                      'cogs.web',
                       'jishaku']
 
 if __name__ == '__main__':
